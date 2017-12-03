86c1dac8740f3551c0bfc8689beb0516177d6c61
Zhe Xian Lee <zxlee618@gmail.com>
2017-12-03 13:22:01 +1100

    Initial Commit

---
+++ ACR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ACR
+//
+//  Created by Zhe Xian Lee on 3/12/17.
+//  Copyright © 2017 Zhe Xian Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ACR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ACR
+//
+//  Created by Zhe Xian Lee on 3/12/17.
+//  Copyright © 2017 Zhe Xian Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
